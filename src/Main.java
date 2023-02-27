8ea49aad98cd7b4b9550784f77563a2546c23f52
Ldv236 <Ldv236@yandex.ru>
2023-02-28 00:05:49 +0200

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,89 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
         task4();
-        task5();
-        task6();
-        task7();
-        task8();
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
 
+         int[] mas1 = new int[] {1, 2, 3};
+
+         double [] mas2 = {1.57, 7.654, 9.986};
+
+         boolean [] mas3 = new boolean[2];
+         mas3[0] = true;
+         mas3[1] = false;
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
 
+        int[] mas1 = new int[] {1, 2, 3};
+        for (int i = 0; i < mas1.length; i++) {
+            System.out.print(mas1[i]);
+            if (i < mas1.length - 1) System.out.print(", ");
+        }
+        System.out.println();
+
+        double [] mas2 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < mas2.length; i++) {
+            System.out.print(mas2[i]);
+            if (i < mas2.length - 1) System.out.print(", ");
+        }
+        System.out.println();
+
+        boolean [] mas3 = new boolean[2];
+        mas3[0] = true;
+        mas3[1] = false;
+        for (int i = 0; i < mas3.length; i++) {
+            System.out.print(mas3[i]);
+            if (i < mas3.length - 1) System.out.print(", ");
+        }
+        System.out.println();
     }
 
     public static void task3 () {
         System.out.println("Задача 3");
 
+        int[] mas1 = new int[] {1, 2, 3};
+        for (int i = mas1.length - 1; i >= 0; i--) {
+            System.out.print(mas1[i]);
+            if (i > 0) System.out.print(", ");
+        }
+        System.out.println();
+
+        double [] mas2 = {1.57, 7.654, 9.986};
+        for (int i = mas2.length - 1; i >= 0; i--) {
+            System.out.print(mas2[i]);
+            if (i > 0) System.out.print(", ");
+        }
+        System.out.println();
+
+        boolean [] mas3 = new boolean[2];
+        mas3[0] = true;
+        mas3[1] = false;
+        for (int i = mas3.length - 1; i >= 0; i--) {
+            System.out.print(mas3[i]);
+            if (i > 0) System.out.print(", ");
+        }
+        System.out.println();
     }
 
     public static void task4 () {
         System.out.println("Задача 4");
 
-    }
-
-    public static void task5 () {
-        System.out.println("Задача 5");
-
-    }
-
-    public static void task6 () {
-        System.out.println("Задача 6");
-
-    }
-
-    public static void task7 () {
-        System.out.println("Задача 7");
-
-    }
-
-    public static void task8 () {
-        System.out.println("Задача 8");
-
+        int[] mas1 = new int[] {1, 2, 3};
+        for (int i = 0; i < mas1.length; i++) {
+            if (mas1[i] % 2 != 0) {
+                mas1[i] ++;
+            }
+        }
+        System.out.println(Arrays.toString(mas1));
     }
 }
